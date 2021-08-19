1d0bfd739b7999ac34033cc2787eb4caacde42c7
JM Rohwer <j.m.rohwer@gmail.com>
2021-08-19 16:23:41 +0200

    update conf.py to include module files in sys.path for doc build

--- source/conf.py
+++ source/conf.py
@@ -18,8 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.join(os.path.expanduser('~'), 'src', 'git', 'pysces', 'pysces'))
-print(sys.path)
+sys.path.insert(0, '../..')
 import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
