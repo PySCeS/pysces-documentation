b6ffef2dcaa0b64e21693c8168249f8017368e05
JM Rohwer <j.m.rohwer@gmail.com>
2024-02-13 22:10:24 +0200

    update Sphinx version for RTD build

--- source/conf.py
+++ source/conf.py
@@ -105,7 +105,7 @@ html_static_path = ['_static']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "PySCeS User Guide v1.1"
+html_title = "PySCeS User Guide v1.2"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = "PySCeS"
